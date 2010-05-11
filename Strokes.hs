8f611906621c5b655159133908f06bf6f192bfca
Daniel Kirsch <danishkirel@gmail.com>
2010-05-11 16:09:17 +0200

    clean and better transmogrify

--- Strokes.hs
+++ Strokes.hs
@@ -3,7 +3,8 @@ module Strokes
   Point(..), Stroke, Strokes, Points,
   add, sub, dot, scalar, norm,
   euclideanDistance, manhattanDistance, dist,
-  boundingbox, refit, redistribute, redistribute'
+  boundingbox, refit, redistribute, redistribute', multiredistribute,
+  limitStrokes, cleanStrokes, removeEmptyStrokes
   ) where
 
 import Data.List (sort, sortBy)
@@ -110,6 +111,7 @@ refit (x1, y1, x2, y2) stroke = for stroke $ \p -> (scale scaleX scaleY (p `sub`
 
 -- remove successive duplicate points
 clean :: Stroke -> Stroke
+clean [] = []
 clean [p] = [p]
 clean s@(p:q:ps) | p == q = clean (p:ps)
                  | otherwise = p:(clean (q:ps))
@@ -141,48 +143,45 @@ redistribute num stroke = redist num $ clean stroke where
       delta = 1e-10
 
 lengths = map slength
-sslength = (foldl1 (+)) . lengths -- lengths of multiple strokes
-toFracs :: Int -> Strokes -> [Double]
-toFracs num l = map ((* (fromIntegral num)).(/ len)) (lengths l) where
-  len = sslength l -- FIXME what if len is 0?
-  
-toIndexed = zip [0..]
-fromIndexed = snd.unzip
-sortBySnd l = ((map flip) . sort . (map flip)) l where
-  flip (a, b) = (b, a)
+fixLengths = map fixLength where -- turn zero lengths into somethin small but non-zero
+  fixLength 0 = 1e-10
+  fixLength l = l
 
--- transmogrify distributes <total> points proportionally over an array of Doubles
+-- transmogrify distributes <points> proportionally over an array of Doubles
 transmogrify :: Int -> [Double] -> [Int]
-transmogrify points = fromIndexed . sort . unflunge . transmogrify' . flunge . sortBySnd . toIndexed . rescale where
+transmogrify points = unflunge . transmogrify' . flunge . rescale where
   rescale :: [Double] -> [Double]
   rescale l = map ((/(foldl1 (+) l)).(*(fromIntegral points))) l
-  flunge :: [(Int, Double)] -> [(Int, Double, Int)]
-  flunge = map (\(a,b) -> (a,b,0))
-  unflunge :: [(Int, Double, Int)] -> [(Int, Int)]
-  unflunge = map (\(a,_,b) -> (a,b))
+  flunge :: [Double] -> [(Double, Int)]
+  flunge = map (\a -> (a,0))
+  unflunge :: [(Double, Int)] -> [Int]
+  unflunge = map (\(_,b) -> b)
   -- transmogrify' repeatedly walks the list
-  transmogrify' :: [(Int, Double, Int)] -> [(Int, Double, Int)]
+  transmogrify' :: [(Double, Int)] -> [(Double, Int)]
   transmogrify' l | left > 0 = transmogrify' $ transmogrify'' left l
                   | otherwise = l where
-                      left = points - (((foldl1 (+)) . fromIndexed . unflunge) l)
+                      left = points - (((foldl1 (+)) . unflunge) l)
   -- transmogrify'' walks the list once
-  transmogrify'' :: Int -> [(Int, Double, Int)] -> [(Int, Double, Int)]
+  transmogrify'' :: Int -> [(Double, Int)] -> [(Double, Int)]
   transmogrify'' 0 l = l
   transmogrify'' _ [] = []
-  transmogrify'' left ((i,r,n):ls) | (ceiling r) > n = (i,r,n+1):(transmogrify'' (left-1) ls)
-                                   | otherwise = (i,r,n):(transmogrify'' left ls)
--- sortByIndex = sort
--- unwrap again
-unwrap = map (fst.snd)
+  transmogrify'' left ((r,n):ls) | (ceiling r) > n = (r,n+1):(transmogrify'' (left-1) ls)
+                                 | otherwise = (r,n):(transmogrify'' left ls)
 redistributeEach :: [Int] -> Strokes -> Strokes
 redistributeEach = zipWith redistribute
 
--- multiredistribute :: Int -> Strokes -> Strokes
--- multiredistribute num strokes = redistributeEach nums strokes where
---   nums = unwrap $ flunge num $ sortByFrac $ toIndexedProperFracList $ toFracList num strokes
+-- beware! fails on empty strokes
+multiredistribute :: Int -> Strokes -> Strokes
+multiredistribute num strokes | num < length strokes =
+  error "Multiredistribute doesn't work with num smaller than the number of strokes" 
+multiredistribute num strokes = redistributeEach nums strokes where
+  nums = transmogrify num $ fixLengths $ lengths strokes
 
--- redistribute each so that the concatenation of the results has n points
--- multiredistribute :: Int -> Strokes -> Strokes
--- multiredistribute num strokes = [] where
---   lengths = map slength strokes
---   totalLength = foldl1 + lengths
+removeEmptyStrokes :: Strokes -> Strokes
+removeEmptyStrokes = filter (/=[])
+
+limitStrokes :: Int -> Strokes -> Strokes
+limitStrokes = take
+
+cleanStrokes :: Strokes -> Strokes
+cleanStrokes = map clean
--- TestStrokes.hs
+++ TestStrokes.hs
@@ -22,18 +22,29 @@ prop_stroke_fits_inside_bounding_box stroke = stroke /= [] ==> forAll_ stroke $ 
 
 prop_refit_fits_inside :: (Double, Double, Double, Double) -> Stroke -> Property
 prop_refit_fits_inside box stroke =
-  stroke /= [] ==> validbox box ==> forAll_ (refit box stroke) $ \p ->
-    p `inside` box
+  stroke /= [] ==> validbox box ==> forAll_ (refit box stroke) $ \p -> p `inside` box
     
 prop_refit_idempotent :: (Double, Double, Double, Double) -> Stroke -> Property
 prop_refit_idempotent box stroke =
   stroke /= [] ==> validbox box ==> refit box stroke ~~ refit box (refit box stroke)
 
+prop_redistribute_works :: Int -> Stroke -> Property
+prop_redistribute_works n stroke =
+  length stroke > 0 ==> n > 0 ==> length (redistribute n stroke) == n
+  
+prop_multiredistribute_works :: Int -> Strokes -> Property
+prop_multiredistribute_works n strokes =
+  length strokes > 0 ==>
+  n > length strokes ==>
+  all (\s -> length s > 0) strokes ==>
+  foldl1 (+) (map length (multiredistribute n strokes)) == n
 
 main = do
  runTests "Stroke" defOpt
   [
   run prop_stroke_fits_inside_bounding_box,
   run prop_refit_fits_inside,
-  run prop_refit_idempotent
+  run prop_refit_idempotent,
+  run prop_redistribute_works,
+  run prop_multiredistribute_works
   ]
